7c9b546ea08389df513740f635184669f24e25a5
Quill Cronwall <quilljazz@gmail.com>
2021-08-13 16:12:37 +0900

    fixes css

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -22,12 +22,12 @@ class ListsController < ApplicationController
   private
 
     # Use callbacks to share common setup or constraints between actions.
-    def set_list
-      @list = List.find(params[:id])
-    end
+  def set_list
+    @list = List.find(params[:id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def list_params
-      params.require(:list).permit(:name, :photo)
-    end
+  # Only allow a list of trusted parameters through.
+  def list_params
+    params.require(:list).permit(:name, :photo)
+  end
 end
