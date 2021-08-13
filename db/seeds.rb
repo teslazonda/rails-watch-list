2c37caf3e0561b80f6eb2018ce2e42e6c7041b4e
Quill Cronwall <quilljazz@gmail.com>
2021-08-13 12:15:53 +0900

    adds create list button

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,9 @@ data['results'].each do |movie|
   puts movie
   Movie.create(title: movie['original_title'], overview: movie['overview'], poster_url: "#{image_base_url}#{movie['poster_path']}", rating: movie['vote_average'])
 end
+
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
